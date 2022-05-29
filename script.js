639ee3fecafe7d6cdb4e5027137a0cbbac8ecf89
RuiOkazaki <okazaki200226@gmail.com>
2022-05-30 00:11:40 +0900

    update: level4はキモすぎた

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const colorChange = () => {
   const shibafu = document.querySelectorAll('.ContributionCalendar-day');
   shibafu.forEach((kusa) =>
     kusa.dataset.level !== '0' && kusa.dataset.date
-      ? (kusa.dataset.level = '4')
+      ? (kusa.dataset.level = '2')
       : null
   );
 };
